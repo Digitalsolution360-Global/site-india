8e64def3406ffeeb77c127e8abbb437fdbc27c28
Digitalsolution360-Global <globalweb3600@gmail.com>
2026-03-01 21:11:50 +0530

    Integrate JSON-LD for product schema in page.js
    Added JSON-LD structured data for SEO enhancement.

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -2,9 +2,31 @@ import CityClient from "./cityClient";
 
 export default function Page() {
 
-  return (
+  const jsonLd = {
+    "@context": "https://schema.org/",
+    "@type": "Product",
+    "name": "Digital Solution",
+    "image": "https://www.digitalsolution360.in/public/frontend/img/logo.png",
+    "description": "Discover professional website development services tailored to your business needs. Reach our experts at +91 99905 56217 and ensure top-notch web solutions.",
+    "brand": {
+      "@type": "Brand",
+      "name": "Digital Solution"
+    },
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "4.5",
+      "reviewCount": "1483"
+    }
+  };
 
-      <CityClient/>
-  
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+
+      <CityClient />
+    </>
   );
 }
