9a508ef895046fa9e00e7240ad3e27bb31fdba5a
Arpit994 <arpitkumar994@gmail.com>
2026-04-10 14:59:30 +0530

    website-development-bangalore google map added

--- app/[slug]/cityClient.js
+++ app/[slug]/cityClient.js
@@ -1216,7 +1216,31 @@ export default function CityClientPage() {
                     </div>
                 </section>
             )}
-
+            {/* Google Map */}
+            {city.city_id == 773 && (  
+                     <section className='py-20 px-4 md:px-8 lg:px-16 bg-white'>
+                    <div className='max-w-4xl mx-auto'>                  
+                    <motion.div
+                        initial={{ opacity: 0, y: 20 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: true }}
+                        transition={{ duration: 0.8 }}
+                        className='rounded-lg overflow-hidden shadow-xl border border-gray-200'
+                    >
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.1782574198432!2d77.69020557389437!3d13.024317787295876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae110026f5869b%3A0x3b2619324f4a4130!2sDigitalsolution360!5e0!3m2!1sen!2sin!4v1775812948628!5m2!1sen!2sin"
+                            width="100%"
+                            height="500"
+                            style={{ border: 0 }}
+                            allowFullScreen=""
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                            title="Digitalsolution360 Office Location"
+                        />
+                    </motion.div>
+                    </div>
+                    </section>
+            )}
             {/* FAQ Section */}
             {faqs.length > 0 && (
                 <section className='py-20 px-4 md:px-8 lg:px-16 bg-white'>
